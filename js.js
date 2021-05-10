2786c93407070c4671ea2d0da09e3531b3a09e55
yogeshkattel <yogeshkattel66@gmail.com>
2021-05-11 01:55:18 +0545

    first commit

---
+++ js.js
@@ -0,0 +1,28 @@
+function sn1(){
+    var audio = new Audio ('str6.wav');
+    audio.play();
+    
+}
+function sn2(){
+    var audio = new Audio ('str5.wav')
+    audio.play();
+}
+function sn3(){
+    var audio = new Audio ('str4.wav')
+    audio.play();
+}
+function sn4(){
+    var audio = new Audio ('str3.wav')
+    audio.play();
+
+}function sn5(){
+    var audio = new Audio ('str2.wav')
+    audio.play();
+}
+function sn6(){
+    var audio = new Audio ('str1.wav')
+    audio.play();
+}
+function lead(){
+    flip
+}
